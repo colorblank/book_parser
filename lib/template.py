4444df5b017b0055b14c0fd5fbfde8af4059e04e
colorblank <jie05200@gmail.com>
2019-02-04 13:30:09 +0800

    template file change

--- lib/template.py
+++ lib/template.py
@@ -13,12 +13,12 @@ import time
 #     latest_chapter = []  # 最新章节
 #     novel_url = ''       # 小说链接
 
-SITE_URL = 'https://www.zwda.com'
-
-search_url = 'https://www.zwda.com/search.php?keyword='
-
-keyword = '重生'
-
+SITE_URL = ''
+# https://www.zwda.com
+search_url = ''
+# https://www.zwda.com/search.php?keyword=
+keyword = ''
+# 重生
 header = [{'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/46.0.2490.76 Mobile Safari/537.36'},
           {'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-us) AppleWebKit/534.50 (KHTML, like Gecko) Version/5.1 Safari/534.50'},
           {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.0; Trident/4.0)'},
